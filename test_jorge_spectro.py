0408a593678bc981c1028454e0bb773e4afec66c
gregoireaufort <gregoire.aufort@gmail.com>
2021-10-01 18:01:09 +0200

    updating spectro

--- test_jorge_spectro.py
+++ test_jorge_spectro.py
@@ -8,42 +8,58 @@ Created on Thu Sep  2 15:59:45 2021
 
 
 
+
 import SED_statistical_analysis
-import importlib
 import scipy.stats as stats
 from utils import *
 import pcigale.sed_modules
-from GMM import GMM_fit, Mixture_t, Mixture_gaussian,multivariate_t
-
+from GMM import Mixture_gaussian
+from pcigale.warehouse import SedWarehouse
+from pcigale.data import Database
+import pandas as pd
+import astropy
 import numpy as np
+
 np.random.seed(42)
 
 
+A=astropy.io.fits.open("/home/aufort/Desktop/jorge/results.fits")
+B = A[1].data
+galaxy_targ = B[0]
+
+
+
+fit_jorge = {"tau_main" : galaxy_targ["best.sfh.tau_main"],
+                'age_main':galaxy_targ["best.sfh.age_main"],
+                  'tau_burst':galaxy_targ["best.sfh.tau_burst"],
+                  'f_burst':galaxy_targ["best.sfh.f_burst"],
+                  'age_burst':galaxy_targ["best.sfh.age_burst"],
+                  'E_BV_lines':galaxy_targ["best.attenuation.E_BV_lines"]}
 
 
 galaxy_obs = SED_statistical_analysis.read_galaxy_fits("observations.fits", 
-                 "222172_best_model.fits",
-                 ident = 222172)
+                 str(galaxy_targ["id"])+"_best_model.fits",
+                 ident = galaxy_targ["id"])
 
 
 
 bands = list(galaxy_obs["bands"])
-#pcigale.sed_modules.get_module('deep_bc03_pca_norm')
-module_list = ['sfhdelayed', 'bc03','nebular','dustatt_modified_starburst','dl2014', 'redshifting']
+pcigale.sed_modules.get_module('deep_bc03_pca_norm')
+module_list = ['deep_sfhdelayed', 'deep_bc03_pca_norm','nebular','dustatt_modified_starburst','dl2014', 'redshifting']
 path_deep = '/home/aufort/Desktop/cigale-master/params_comparison.txt'
-file_store = 'store_parameters_test_spectro_prior_jorge.csv'
-deep_modules = None#[pcigale.sed_modules.deep_bc03_pca_norm]
+file_store = 'store_parameters_'+str(galaxy_targ["id"])+'_deep.csv'
+deep_modules = [pcigale.sed_modules.deep_bc03_pca_norm]
 module_parameters_to_fit = {'tau_main': {"type":"unif","min":1500,"max" :3000},
             'age_main': {"type":"unif","min":1000,"max" :10000},
             'tau_burst':{"type":"unif","min":100,"max" :10000},
             'f_burst': {"type":"unif","min":0,"max" :0.2},
             'age_burst': {"type":"log","min":1,"max" :2},
+            'E_BV_lines' : {"type":"unif","min":0,"max" :2},
     
 }
 
 module_parameters_discrete = {'sfr_A' : [1.],
                              'normalise' : [True],
-                             'E_BV_lines' : [0.1],
                              'E_BV_factor' :  [0.44],
                              'uv_bump_wavelength' : [217.5],
                              'uv_bump_width' :[35.0],
@@ -67,33 +83,7 @@ module_parameters_discrete = {'sfr_A' : [1.],
                              'filters':["B_B90 & V_B90 & FUV"],
 }
 wavelength_limits = {"min" : 645,"max" : 1800}
-wavelength_lines =[121.60000000000001,
- 133.5,
- 139.70000000000002,
- 154.9,
- 164.0,
- 166.5,
- 190.9,
- 232.60000000000002,
- 279.8,
- 372.70000000000005,
- 379.8,
- 383.5,
- 386.90000000000003,
- 388.90000000000003,
- 397.0,
- 407.0,
- 410.20000000000005,
- 434.0,
- 486.1,
- 495.90000000000003,
- 500.70000000000005,
- 630.0,
- 654.8000000000001,
- 656.3000000000001,
- 658.4000000000001,
- 671.6,
- 673.1]
+wavelength_lines =[121.60000000000001,133.5,139.7, 154.9, 164.0, 166.5, 190.9,232.6, 279.8, 372.7, 379.8, 383.5, 386.9, 388.9, 397.0, 407.0, 410.2, 434.0, 486.1, 495.9, 500.7, 630.0, 654.8,656.3, 658.4, 671.6, 673.1]
 nebular_params = {"lines_width" : module_parameters_discrete["lines_width"][0],"line_waves" : wavelength_lines}
 CIGALE_parameters = {"module_list":module_list,
                     "path_deep" : path_deep,
@@ -106,7 +96,7 @@ CIGALE_parameters = {"module_list":module_list,
                     "nebular" :nebular_params,
                     "bands" :bands,
                     "mode" : ["spectro"],
-                    "n_jobs" : 12}
+                    "n_jobs" : 10}
 
 dim_prior = len(CIGALE_parameters["module_parameters_to_fit"]) #Number of continuous parameters to fit
 n_comp = 4 #arbitrary
@@ -136,8 +126,38 @@ TAMIS_parameters = {'dim_prior' : dim_prior,
                     "verbose" : True
     
 }
-result = SED_statistical_analysis.fit(galaxy_obs , CIGALE_parameters, TAMIS_parameters)
-SED_statistical_analysis.analyse_results(CIGALE_parameters)
+# result = SED_statistical_analysis.fit(galaxy_obs,
+#                                       CIGALE_parameters, TAMIS_parameters)
+
+
+
+# SED_statistical_analysis.plot_result(CIGALE_parameters,
+#                                      line_dict_fit = fit_jorge,
+#                                      title = "Deep jorge 1")
+
+
+
+module_list_normal = ['sfhdelayed', 'bc03','nebular','dustatt_modified_starburst','dl2014', 'redshifting']
+file_store_normal = 'store_parameters_'+str(galaxy_targ["id"])+'_spectro_normal.csv'
+
+CIGALE_parameters_normal = {"module_list":module_list_normal,
+                    "path_deep" : path_deep,
+                    "file_store":file_store_normal,
+                    "deep_modules":None,
+                    "module_parameters_to_fit":module_parameters_to_fit,
+                    "module_parameters_discrete":module_parameters_discrete,
+                    "n_bins":10,
+                    "wavelength_limits" : wavelength_limits,
+                    "nebular" :nebular_params,
+                    "bands" :bands,
+                    "mode" : ["spectro"],
+                    "n_jobs" : 10}
+result_normal = SED_statistical_analysis.fit(galaxy_obs , CIGALE_parameters_normal, TAMIS_parameters)
+
+SED_statistical_analysis.plot_result(CIGALE_parameters_normal,
+                                     line_dict_fit = fit_jorge,
+                                     title = "CIGALE Jorge 1 spectro")
+# SED_statistical_analysis.analyse_results(CIGALE_parameters_normal)
 
 
 
