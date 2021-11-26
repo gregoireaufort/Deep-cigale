fixing nebular normalization

data/deep_cloudy.py
@@ -8,9 +8,7 @@ import pandas as pd
 import  joblib
 import numpy as np
 
-
-
-def Deep_cloudy(params_cloudy,path_cigale):
+def Deep_cloudy(params_cloudy):
     """Simulates part of Cloudy's lines output through a deep neural network
     
     Input:
@@ -21,20 +19,21 @@ def Deep_cloudy(params_cloudy,path_cigale):
                     (unnormalized)
     """
     
-    X = params_cloudy.copy()
-    X['deep_nebular.Age'] /=1e6
-    scaler = joblib.load(path_cigale+"X_scaling.pkl")
+    X = params_cloudy
+    X['com3'] /=1e6
+    scaler = joblib.load("data/X_scaling.pkl")
     X= scaler.transform(X)
     
-    pca = joblib.load(path_cigale+"pca_trained.pkl")
-    ANN = joblib.load(path_cigale+"ANN/13_04_27_06.pkl")
+    pca = joblib.load("data/pca_trained.pkl")
+    ANN = joblib.load("ANN/13_04_27_06.pkl")
     y_pred = ANN.predict(X)
-    ANN_hbeta = joblib.load(path_cigale+"data/ANN/ANN_hbeta_256.pkl")
+    ANN_hbeta = joblib.load("ANN/ANN_hbeta_256.pkl")
     Hb = np.exp(ANN_hbeta.predict(X))
     norm_lines = pca.inverse_transform(y_pred)
-        
+    ANN_logQ = joblib.load("ANN/ANN_logQ.pkl")
+    logQ = 10**ANN_logQ.predict(X)
     line_names = []
-    with open(path_cigale+'list_lines.txt', 'r') as filehandle:
+    with open('data/list_lines.txt', 'r') as filehandle:
         for line in filehandle:
             currentPlace = line[:-1]
             line_names.append(currentPlace)
@@ -42,6 +41,7 @@ def Deep_cloudy(params_cloudy,path_cigale):
     lines_df= pd.DataFrame(data = np.exp(norm_lines),columns= line_names)
     lines_df2 = lines_df.multiply(Hb,axis = 0)
     lines_df2["H__1_486133A"] = Hb
+    lines_df2.multiply(logQ, axis = 0)
     
     
     
@@ -50,9 +50,8 @@ def Deep_cloudy(params_cloudy,path_cigale):
 
 
 if __name__ == "__main__":
-    path_data = "/home/aufort/Desktop/Deep-cigale/pcigale/data/"
     params_cloudy = pd.read_csv("params_cloudy.csv",index_col = 0)
-    pred_lines = Deep_cloudy(params_cloudy,path_data)
+    pred_lines = Deep_cloudy(params_cloudy)
     pred_lines.to_csv("lines.csv")
     
     

sed_modules/deep_nebular.py
@@ -36,7 +36,7 @@ class NebularEmission(SedModule):
     params_nebular = params[names_deep_neb]
     path_cloudy =os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/data/'
 
-    all_lines = deep_cloudy.Deep_cloudy(params_nebular,path_cloudy )/(4*np.pi*(3.086*10**21)**2) #F = L/ 4*pi*D^2, D = 3.086*10*21 cm, 
+    all_lines = deep_cloudy.Deep_cloudy(params_nebular,path_cloudy )#/(4*np.pi*(3.086*10**21)**2) #F = L/ 4*pi*D^2, D = 3.086*10*21 cm, 
     all_lines /= 1e7 #Erg/S to W
     n = params_nebular.shape[0]
     #-------------------- Continuum part, to be added later -------#